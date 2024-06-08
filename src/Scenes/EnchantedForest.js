af19f7c2a4c30c47bbd37c34d9237c0697997a23
Sandra Foss Sørensen <202106467@post.au.dk>
2024-06-08 10:18:54 -0700

    Working on map

--- src/Scenes/EnchantedForest.js
+++ src/Scenes/EnchantedForest.js
@@ -3,8 +3,7 @@ class EnchantedForest extends Phaser.Scene {
         super("enchantedForestScene");
     }
 
-    preload() {
-    }
+    preload() { }
 
     init() {
         this.TILESIZE = 16;
@@ -18,21 +17,35 @@ class EnchantedForest extends Phaser.Scene {
         this.map = this.add.tilemap("enchantedForest", this.TILESIZE, this.TILESIZE, this.TILEHEIGHT, this.TILEWIDTH);
         this.physics.world.setBounds(0, 0, 200 * 18, 25 * 18);
 
-        // Add a tileset to the map
-        this.tileset = this.map.addTilesetImage("kenney-tiny-town", "tilemap_tiles");
+        // Add a tileset to the map using the correct key
+        this.tileset = this.map.addTilesetImage("colored_tilemap_packed", "colored_tilemap_tiles");
 
         // Create the layers
         this.groundLayer = this.map.createLayer("Ground-n-Walkways", this.tileset, 0, 0);
         this.treesLayer = this.map.createLayer("Trees-n-Bushes", this.tileset, 0, 0);
         this.housesLayer = this.map.createLayer("Houses-n-Fences", this.tileset, 0, 0);
 
+        this.groundLayer.setCollisionByProperty({ collides: true });
+
         // Create townsfolk sprite
         // Use setOrigin() to ensure the tile space computations work well
-        my.sprite.purpleTownie = this.add.sprite(this.tileXtoWorld(5), this.tileYtoWorld(5), "purple").setOrigin(0, 0);
+        this.purpleTownie = this.add.sprite(this.tileXtoWorld(5), this.tileYtoWorld(5), "purple").setOrigin(0, 0);
+
+        // Create orcs
+        this.weakOrc = this.physics.add.sprite(this.tileXtoWorld(25), this.tileYtoWorld(15), "weakOrch").setOrigin(0,0);
 
         // Enable physics for the sprite without debug visuals
-        this.physics.add.existing(my.sprite.purpleTownie);
-        my.sprite.purpleTownie.body.setCollideWorldBounds(true);
+        this.physics.add.existing(this.purpleTownie);
+        this.purpleTownie.body.setCollideWorldBounds(true);
+
+        // Enable collision handling
+        this.physics.add.collider(this.purpleTownie, this.groundLayer, () => {
+        });
+        this.physics.add.collider(this.weakOrc, this.groundLayer);
+
+
+        // Create sword sprite using a frame from the spritesheet
+        this.sword = this.add.sprite(0, 0, "colored_tilemap_sheet", 70).setOrigin(0.5, 0.5);
 
         // Camera settings
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
@@ -40,10 +53,10 @@ class EnchantedForest extends Phaser.Scene {
         this.cameras.main.setZoom(this.SCALE);
 
         // Add camera follow to the sprite
-        this.cameras.main.startFollow(my.sprite.purpleTownie, true, 0.25, 0.25); // (target, [,roundPixels][,lerpX][,lerpY])
+        this.cameras.main.startFollow(this.purpleTownie, true, 0.25, 0.25); // (target, [,roundPixels][,lerpX][,lerpY])
         this.cameras.main.setDeadzone(50, 50);
 
-        this.activeCharacter = my.sprite.purpleTownie;
+        this.activeCharacter = this.purpleTownie;
 
         // Add key handlers for arrow keys
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -51,25 +64,37 @@ class EnchantedForest extends Phaser.Scene {
         // Find the port to next level in the "Objects" layer in Phaser
         this.leftPort = this.map.createFromObjects("Objects", {
             name: "leftPort",
-            key: "tilemap_sheet",
+            key: "colored_tilemap_sheet",
             frame: 23
-        })
+        });
 
         this.rightPort = this.map.createFromObjects("Objects", {
             name: "rightPort",
-            key: "tilemap_sheet",
+            key: "colored_tilemap_sheet",
             frame: 22
-        })
+        });
+
+        this.fire = this.map.createFromObjects("Objects", {
+            name: "fire",
+            key: "colored_tilemap_sheet",
+            frame: 136
+        });
+ 
+        this.key = this.map.createFromObjects("Objects", {
+            name: "key",
+            key: "colored_tilemap_sheet",
+            frame: 90
+        });
 
         // Enable collision handling
         this.physics.world.enable(this.leftPort, Phaser.Physics.Arcade.STATIC_BODY);
         this.physics.world.enable(this.rightPort, Phaser.Physics.Arcade.STATIC_BODY);
 
-        this.physics.add.overlap(my.sprite.purpleTownie, this.leftPort, (obj1, obj2) => {
+        this.physics.add.overlap(this.purpleTownie, this.leftPort, (obj1, obj2) => {
             this.scene.start("mysticalCastle");
         });
 
-        this.physics.add.overlap(my.sprite.purpleTownie, this.rightPort, (obj1, obj2) => {
+        this.physics.add.overlap(this.purpleTownie, this.rightPort, (obj1, obj2) => {
             this.scene.start("mysticalCastle");
         });
     }
@@ -78,8 +103,7 @@ class EnchantedForest extends Phaser.Scene {
         // Reset velocity
         this.activeCharacter.body.setVelocity(0);
 
-        if(this.cursors.space.isDown)
-        {
+        if (this.cursors.space.isDown) {
             this.activeCharacter.body.setVelocityY(-80);
         }
 
@@ -95,6 +119,19 @@ class EnchantedForest extends Phaser.Scene {
         } else if (this.cursors.down.isDown) {
             this.activeCharacter.body.setVelocityY(100);
         }
+
+        // Update sword position relative to the character
+        this.updateSwordPosition();
+    }
+
+    updateSwordPosition() {
+        // Adjust these offsets to position the sword correctly
+        const swordOffsetX = this.activeCharacter.flipX ? -5 : 5; // Adjust this value to position the sword horizontally
+        const swordOffsetY = 5;  // Adjust this value to position the sword vertically
+
+        // Update the sword position
+        this.sword.x = this.activeCharacter.x + swordOffsetX;
+        this.sword.y = this.activeCharacter.y + swordOffsetY;
     }
 
     tileXtoWorld(tileX) {
--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -9,13 +9,25 @@ class Load extends Phaser.Scene {
         // Load townsfolk
         this.load.image("purple", "purple_townie.png");
         this.load.image("blue", "blue_townie.png");
+        this.load.image("weakOrch", "tile_0004.png");
 
+
+        // Load tilemap images with unique keys
+        this.load.image("colored_tilemap_tiles", "colored_tilemap_packed.png");
+        this.load.spritesheet("colored_tilemap_sheet", "colored_tilemap_packed.png", {
+            frameWidth: 8,
+            frameHeight: 8
+        });
+        
         // Load tilemap information
         this.load.image("tilemap_tiles", "tilemap_packed.png"); 
         this.load.spritesheet("tilemap_sheet", "tilemap_packed.png", {
             frameWidth: 16,
             frameHeight: 16
         });                 // Packed tilemap
+
+
+
         this.load.tilemapTiledJSON("mysticalCastle", "mysticalCastle.tmj");   // Tilemap in JSON
         this.load.tilemapTiledJSON("darkCave", "darkCave.tmj");
         this.load.tilemapTiledJSON("enchantedForest", "enchantedForest.tmj");   // Tilemap in JSON
@@ -26,7 +38,7 @@ class Load extends Phaser.Scene {
         
 
          // ...and pass to the next Scene
-         this.scene.start("mysticalCastleScene");
+         this.scene.start("enchantedForestScene");
     }
 
     // Never get here since a new scene is started in create()
